1f46e8d545f5a28da3ae130f056a336e81815321
maxwelltram <56807154+maxwelltram@users.noreply.github.com>
2021-12-18 22:32:47 -0600

    Agregamos scripts y el .sh

---
+++ Practica1/Practica1/CreacionTablasTemp.sql
@@ -0,0 +1,81 @@
+CREATE TEMPORARY TABLE country_codes_temp(
+    campo_1 varchar(250) NULL,
+    campo_2 varchar(250) NULL,
+    campo_3 varchar(250) NULL,
+    campo_4 int NULL,
+    campo_5 varchar(250) NULL,
+    campo_6 varchar(250) NULL,
+    campo_7 int NULL,
+    campo_8 int NULL,
+    campo_9 varchar(250) NULL,
+    campo_10 int NULL,
+    campo_11 int NULL,
+    campo_12 varchar(250) NULL,
+    campo_13 varchar(250) NULL,
+    campo_14 int NULL,
+    campo_15 int NULL,
+    campo_16 varchar(250) NULL,
+    campo_17 int NULL,
+    campo_18 varchar(250) NULL
+);
+
+CREATE TEMPORARY TABLE project_temp(
+    campo_1 int NULL,
+    campo_2 varchar(250) NULL,
+    campo_3 int NULL,
+    campo_4 varchar(250) NULL,
+    campo_5 date NULL,
+    campo_6 date NULL,
+    campo_7 varchar(250) NULL,
+    campo_8 varchar(250) NULL,
+    campo_9 int NULL,
+    campo_10 varchar(250) NULL,
+    campo_11 varchar(250) NULL,
+    campo_12 varchar(250) NULL,
+    campo_13 int NULL,
+    campo_14 int NULL,
+    campo_15 int NULL,
+    campo_16 decimal(16,5) NULL,
+    campo_17 decimal(16,5) NULL
+);
+
+CREATE TEMPORARY TABLE locations_temp(
+    campo_1 int NULL,
+    campo_2 varchar(250) NULL,
+    campo_3 varchar(250) NULL
+);
+
+CREATE TEMPORARY TABLE level_temp(
+    campo_1 int NULL,
+    campo_2 int NULL,
+    campo_3 varchar(250) NULL,
+    campo_4 int NULL,
+    campo_5 int NULL,
+    campo_6 int NULL,
+    campo_7 decimal(16,5) NULL,
+    campo_8 decimal(16,5) NULL
+);
+
+CREATE TEMPORARY TABLE transactions_temp(
+    campo_1 int NULL,
+    campo_2 varchar(250) NULL,
+    campo_3 int NULL,
+    campo_4 date NULL,
+    campo_5 date NULL,
+    campo_6 varchar(250) NULL,
+    campo_7 int NULL,
+    campo_8 decimal(16,5) NULL
+);
+
+CREATE TEMPORARY TABLE geonames_temp(
+    campo_1 int NULL,
+    campo_2 int NULL,
+    campo_3 varchar(250) NULL,
+    campo_4 decimal(16,5) NULL,
+    campo_5 decimal(16,5) NULL,
+    campo_6 int NULL,
+    campo_7 varchar(250) NULL,
+    campo_8 varchar(250) NULL,
+    campo_9 int NULL,
+    campo_10 int NULL
+);
---
+++ Practica1/Practica1/Practica1.sh
@@ -0,0 +1,4 @@
+#!/bin/bash
+# -*- ENCODING: UTF-8 -*-
+sudo mysql -u root -p Practica1 <./Eliminacion.sql
+sudo mysql -u root -p Practica1 <./Creacion.sql
